791c9109e96b94c2633c2e9acfdf6a53026b4135
theakshat123 <151052142+theakshat123@users.noreply.github.com>
2026-07-15 06:28:54 +0530

    Time: 3 ms (70.13%) | Memory: 16.4 MB (16.15%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+  void fun(int open,int close, int n, string temp, vector<string>&res){
+    if(open==n && close==n){
+        res.push_back(temp);
+        return;
+    }
+    if(open<n){
+        temp.push_back('(');
+        fun(open+1,close,n,temp,res);
+        temp.pop_back();
+    }
+    if(close<open){
+        temp.push_back(')');
+        fun(open,close+1,n,temp,res);
+        temp.pop_back();
+    }
+  }
+    vector<string> generateParenthesis(int n) {
+        vector<string>res;
+        string temp = "";
+        fun(0,0,n,temp,res);
+      return res;
+    }
+};
